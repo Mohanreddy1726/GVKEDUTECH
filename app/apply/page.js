a7100a2439b14d15b922dcce78cf4ba059137976
Mohanreddy1726 <naradamohan1@gmail.com>
2026-04-18 16:18:12 +0530

    UseStatedefined

--- app/apply/page.js
+++ app/apply/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-// import { useState } from "react";
+import { useState } from "react";
 import { PageLayout } from "@/components/PageLayout";
 import { PageHeader } from "@/components/PageHeader";
 import { Card, CardContent } from "@/components/ui/card";
