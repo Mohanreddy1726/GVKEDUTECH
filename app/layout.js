e7457944f8be1b14059225a13100c08b818a353b
Mohanreddy1726 <naradamohan1@gmail.com>
2026-05-28 17:59:34 +0530

    shiva photo update

--- app/layout.js
+++ app/layout.js
@@ -139,6 +139,7 @@ export default function RootLayout({ children }) {
         <WebSiteSchema />
         <OrganizationSchema />
         {children}
+         
         <Toaster />
       </body>
     </html>
