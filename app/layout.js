d61657d39cc2afaa2d4454941ec5623305fe3aae
Mohanreddy1726 <naradamohan1@gmail.com>
2026-06-23 13:07:28 +0530

    Changed to official gtm integration

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,8 @@
 import { Poppins, Inter } from "next/font/google";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { Toaster } from "@/components/ui/toaster";
 import {
   OrganizationSchema,
-  ArticleSchema,
   WebSiteSchema,
 } from "@/components/SEO";
 import "./globals.css";
@@ -112,39 +111,17 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${poppins.variable} ${inter.variable} h-full antialiased`}
     >
-      <head>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-P62XGZ78');
-          `}
-        </Script>
-      </head>
-
       <body className="min-h-full flex flex-col font-inter">
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P62XGZ78"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
 
         <WebSiteSchema />
         <OrganizationSchema />
+
         {children}
+
         <Toaster />
       </body>
+
+      <GoogleTagManager gtmId="GTM-P62XGZ78" />
     </html>
   );
 }
